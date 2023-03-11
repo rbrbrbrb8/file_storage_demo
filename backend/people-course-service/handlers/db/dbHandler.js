dd75fc0965f55bcfdee79b2b2a092a032b49f16d
Roy Brez <rbrbrbrb8@gmail.com>
2023-03-11 23:04:38 +0200

    started docker and k8s infrastructure

--- backend/people-course-service/handlers/db/dbHandler.js
+++ backend/people-course-service/handlers/db/dbHandler.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 // console.log(process.env)
 
 dbHandler.init = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/protoImages', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect('mongodb://people-course-mongo-service:27017/protoImages', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(res => {console.log('yessirrrr we up')})
+  .catch(err => {console.log(err)})
 }
 
 
